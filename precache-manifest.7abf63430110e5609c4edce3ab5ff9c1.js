888db684ed2d8b9c06703b81562365a63fa3c7bc
swapnilkusumwal <43251574+swapnilkusumwal@users.noreply.github.com>
2020-06-30 09:50:00 +0530

    Updates

---
+++ precache-manifest.7abf63430110e5609c4edce3ab5ff9c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98d1dd534e60c878c7506491f49e7f8",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "9b7a16359b9772715382",
+    "url": "/Drum-Machine/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6fa4f067d0e53c3b5b11",
+    "url": "/Drum-Machine/static/css/main.f0371e53.chunk.css"
+  },
+  {
+    "revision": "9b7a16359b9772715382",
+    "url": "/Drum-Machine/static/js/2.e360587d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Drum-Machine/static/js/2.e360587d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa4f067d0e53c3b5b11",
+    "url": "/Drum-Machine/static/js/main.540b7f18.chunk.js"
+  },
+  {
+    "revision": "0a80a6f3bfa1489656d6",
+    "url": "/Drum-Machine/static/js/runtime-main.1267922b.js"
+  }
+]);
